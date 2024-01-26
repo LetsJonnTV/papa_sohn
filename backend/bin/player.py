f782514b92200ad6f1d1884c500444eb4931e4d3
profiler5000 <profiler5000@gmail.com>
2024-01-26 13:44:09 +0100

    Aktualisieren von player.py Berichtigung Rechtschreibung

--- backend/bin/player.py
+++ backend/bin/player.py
@@ -4,7 +4,7 @@ from user_data import user_res, user
 from res import geld, xp, level
 
 
-class Karakter_Stats:
+class Charakter_Stats:
     def __init__(self, geld, xp, level):
         self.__geld__ = geld
         self.__xp__ = xp
@@ -20,16 +20,16 @@ class Resurcen_Stats:
         return dic
 
 class Player:
-    def __init__(self, username, passwort, res_stats, kar_stats):
+    def __init__(self, username, passwort, res_stats, char_stats):
         self.__username__ = username
         self.__passwort__ = passwort
         self.__res_stats__ = res_stats
-        self.__karakter_stats__ = kar_stats
+        self.__Charakter_stats__ = char_stats
 
     def returnen(self):
-        dic = {"Username": self.__username__, "Passwort": self.__passwort__, "Res_Stats": self.__res_stats__, "Kar_Stats": self.__karakter_stats__}
+        dic = {"Username": self.__username__, "Passwort": self.__passwort__, "Res_Stats": self.__res_stats__, "Char_Stats": self.__Charakter_stats__}
 
 
-main_kar_stats = Karakter_Stats(geld, xp, level)
+main_char_stats = Charakter_Stats(geld, xp, level)
 main_res_stats = Resurcen_Stats(user_res[1][2], user_res[1][3])
-main_player = Player(user[0],user[1], main_res_stats.returnen(), main_kar_stats.returnen())
+main_player = Player(user[0],user[1], main_res_stats.returnen(), main_char_stats.returnen())
